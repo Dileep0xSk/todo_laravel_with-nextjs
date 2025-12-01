3b7b86c4b9df65cd4644fb0baa017f1d29ca940e
Dileep-sk <dileep@skylineinfosys.com>
2025-12-01 20:32:44 +0530

    Initail setup nextjs project

--- todo-api/routes/api.php
+++ todo-api/routes/api.php
@@ -8,4 +8,4 @@ use App\Http\Controllers\Api\SubmissionController;
 Route::post('submissions', [SubmissionController::class, 'store']);
 Route::get('submissions', [SubmissionController::class, 'index']);
 Route::get('submissions/{id}', [SubmissionController::class, 'show']);
-Route::post('submissions/{id}', [SubmissionController::class, 'update']); 
+Route::post('submissions/{id}', [SubmissionController::class, 'update']);
